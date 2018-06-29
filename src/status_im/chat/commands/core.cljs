f38567d75255c53b72bbd3d866a7166adb35b624
janherich <jan.herich@gmail.com>
2018-06-29 14:07:13 +0200

    Cljs commands proposal

---
+++ src/status_im/chat/commands/core.cljs
@@ -0,0 +1,13 @@
+(ns status-im.chat.commands.core
+  (:require [status-im.chat.commands.protocol :as protocol]))
+
+(defn validate-and-send
+  "Validates and sends command in current chat"
+  [command cofx]
+  nil)
+
+(defn send
+  "Sends command with given arguments in particular chat"
+  [command chat-id cofx]
+  nil)
+
