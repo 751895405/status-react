cff0c6c105a5149502d69a87913a96366778b10b
jhe <jan.herich@gmail.com>
2018-07-09 23:40:44 +0200

    WIP

--- src/status_im/chat/commands/core.cljs
+++ src/status_im/chat/commands/core.cljs
@@ -1,5 +1,17 @@
 (ns status-im.chat.commands.core
-  (:require [status-im.chat.commands.protocol :as protocol]))
+  (:require [status-im.chat.commands.protocol :as protocol]
+            [status-im.chat.commands.impl.transactions :as transactions]
+            [status-im.chat.models.input :as input]))
+
+(def ^:private arg-wrapping-char "\"")
+(def ^:private command-char "/")
+(def ^:private space-chat " ")
+
+(def commands-register
+  "Register of all commands. Whenever implementing a new command,
+  provide the implementation in the `status-im.chat.commands.impl.*` ns,
+  and add its instance here."
+  #{(transactions/PersonalSendCommand.)})
 
 (defn validate-and-send
   "Validates and sends command in current chat"
@@ -11,3 +23,9 @@
   [command chat-id cofx]
   nil)
 
+(defn set-command-argument
+  "Set value as command argument for the current chat"
+  [value {:keys [db]}]
+  (let [{:keys [current-chat-id]} db
+        [command & command-args]  (-> (get-in db [:chats current-chat-id :input-text])
+                                      input/split-command-args)]))
--- src/status_im/chat/commands/impl/transactions.cljs
+++ src/status_im/chat/commands/impl/transactions.cljs
@@ -1,14 +1,44 @@
-(ns status-im.chat.commands.impl.send
+(ns status-im.chat.commands.impl.transactions
   (:require-macros [status-im.utils.views :refer [defview letsubs]])
   (:require [re-frame.core :as re-frame]
             [status-im.chat.commands.protocol :as protocol]
             [status-im.ui.components.react :as react]
             [status-im.ui.components.icons.vector-icons :as vector-icons]
             [status-im.ui.components.colors :as colors]
+            [status-im.ui.components.list.views :as list]
             [status-im.i18n :as i18n]
-            [status-im.chat.styles.message.message :as style]))
+            [status-im.chat.commands.impl.transactions.styles :as transactions-styles]
+            [status-im.chat.styles.message.message :as message-styles]))
 
-;; Example of command implementation
+(defn- render-asset [arg-index bot-db-key]
+  (fn [{:keys [name symbol amount decimals] :as asset}]
+    [react/touchable-highlight {:on-press #(re-frame/dispatch
+                                            [:set-asset-as-command-argument {:arg-index  arg-index
+                                                                             :bot-db-key bot-db-key
+                                                                             :asset      (clean-asset asset)}])}
+     [react/view transaction-styles/asset-container
+      [react/view transaction-styles/asset-main
+       [react/image {:source (-> asset :icon :source)
+                     :style  transaction-styles/asset-icon}]
+       [react/text {:style transaction-styles/asset-symbol} symbol]
+       [react/text {:style transaction-styles/asset-name} name]]
+      ;;TODO(goranjovic) : temporarily disabled to fix https://github.com/status-im/status-react/issues/4963
+      ;;until the resolution of https://github.com/status-im/status-react/issues/4972
+      #_[react/text {:style transaction-styles/asset-balance}
+         (str (money/internal->formatted amount symbol decimals))]]]))
+
+(def assets-separator [react/view transaction-styles/asset-separator])
+
+(defview choose-asset-view [on-selected]
+  (letsubs [assets [:wallet/visible-assets-with-amount]]
+    [react/view
+     [list/flat-list {:data                      (filter #(not (:nft? %)) assets)
+                      :key-fn                    (comp name :symbol)
+                      :render-fn                 (render-asset arg-index bot-db-key)
+                      :enableEmptySections       true
+                      :separator                 assets-separator
+                      :keyboardShouldPersistTaps :always
+                      :bounces                   false}]]))
 
 (defn send-short-preview
   [{:keys [content]}]
@@ -24,12 +54,12 @@
             tx-exists? [:wallet-transaction-exists? tx-hash]]
     [react/touchable-highlight {:on-press #(when tx-exists?
                                              (re-frame/dispatch [:show-transaction-details tx-hash]))}
-     [react/view style/command-send-status-container
+     [react/view message-styles/command-send-status-container
       [vector-icons/icon (if confirmed? :icons/check :icons/dots)
        {:color           colors/blue
-        :container-style (style/command-send-status-icon outgoing)}]
+        :container-style (message-styles/command-send-status-icon outgoing)}]
       [react/view
-       [react/text {:style style/command-send-status-text}
+       [react/text {:style message-styles/command-send-status-text}
         (i18n/label (cond
                       confirmed? :status-confirmed
                       tx-exists? :status-pending
@@ -41,31 +71,31 @@
     (let [{{:keys [amount fiat-amount tx-hash asset] send-network :network} :params} content
           recipient-name (get-in content [:params :bot-db :public :recipient])
           network-mismatch? (and (seq send-network) (not= network send-network))]
-      [react/view style/command-send-message-view
+      [react/view message-styles/command-send-message-view
        [react/view
-        [react/view style/command-send-amount-row
-         [react/view style/command-send-amount
-          [react/text {:style style/command-send-amount-text
+        [react/view message-styles/command-send-amount-row
+         [react/view message-styles/command-send-amount
+          [react/text {:style message-styles/command-send-amount-text
                        :font  :medium}
            amount
-           [react/text {:style (style/command-amount-currency-separator outgoing)}
+           [react/text {:style (message-styles/command-amount-currency-separator outgoing)}
             "."]
-           [react/text {:style (style/command-send-currency-text outgoing)
+           [react/text {:style (message-styles/command-send-currency-text outgoing)
                         :font  :default}
             asset]]]]
         (when fiat-amount
-          [react/view style/command-send-fiat-amount
-           [react/text {:style style/command-send-fiat-amount-text}
+          [react/view message-styles/command-send-fiat-amount
+           [react/text {:style message-styles/command-send-fiat-amount-text}
             (str "~ " fiat-amount " " (i18n/label :usd-currency))]])
         (when (and group-chat
                    recipient-name)
-          [react/text {:style style/command-send-recipient-text}
+          [react/text {:style message-styles/command-send-recipient-text}
            (str
             (i18n/label :send-sending-to)
             " "
             recipient-name)])
         [react/view
-         [react/text {:style (style/command-send-timestamp outgoing)}
+         [react/text {:style (message-styles/command-send-timestamp outgoing)}
           (str (i18n/label :sent-at) " " timestamp-str)]]
         [send-status tx-hash outgoing]
         (when network-mismatch?
@@ -80,6 +110,7 @@
   (parameters [_]
     [{:id :asset
       :type :text
+      :placeholder "Currency"
       ;; Suggestion components should be structured in such way that they will just take
       ;; one argument, event-creator fn used to construct event to fire whenever something
       ;; is selected.
